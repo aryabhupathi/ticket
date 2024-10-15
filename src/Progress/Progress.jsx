50cfeb830ef37aac1a74e43b95a7c16d356a18b7
Arya Bhupathi <bhupathikonduru@gmail.com>
2024-10-15 18:09:21 +0530

    ui

--- src/Progress/Progress.jsx
+++ src/Progress/Progress.jsx
@@ -1,26 +1,3 @@
-// import React from "react";
-// import { Box, Typography } from "@mui/material";
-// const Progress = () => {
-//   return (<Box>
-//     <Box
-//       sx={{
-//         display: "flex",
-//         justifyContent: "center",
-//         alignContent: "center",
-//       }}
-//     >
-//       <Typography variant="h3" color="blue">
-//         SITE UNDER CONSTRUCTION
-//       </Typography></Box><Box>
-//       <Typography variant="h2" color="red">
-//         PROCESSING . . . .
-//       </Typography>
-//     </Box></Box>
-//   );
-// };
-
-// export default Progress;
-
 
 import React from "react";
 import { Box, Typography } from "@mui/material";
